090ae12d763ebf92da4d838e6d0d05ced853f43f
madhuresh <minoshideveloper8@gmail.com>
2022-04-12 14:53:29 +0530

    window initialized

--- src/main.c
+++ src/main.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
-// #include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
 
+//GLOBAL variable
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL ;
+bool is_running = false;
+
+bool initialize_window(void)
+{
+    //initializing hardware
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr,"Error initializing SDL.\n");
+        return false;
+    }
+    //create a sdl window
+    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
+    if (!window)
+    {
+        fprintf(stderr, "Error creating SDL windoe.\n");
+        return false;
+    }
+
+    // create SDl renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer)
+    {
+        fprintf(stderr, "Error creating renderer\n");
+        return false;
+    }
+
+    return true;
+}
 int main(void)
 {
+    is_running = initialize_window();
 
-    printf("Hello World!\n");
 
-    // return EXIT_SUCCESS;
+    
+
+    
     return 0;
 }
